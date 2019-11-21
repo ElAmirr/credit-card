c40474702727c6ecedca96d08b999e3da8ee6d66
ElAmirr <othmaniamir41gitgit config --global user.name ElAmirrgit config --global user.email othmaniamir41@gmail.com git config --global user.email othmaniamir41git>
2019-11-21 14:37:13 +0100

    Updates

---
+++ precache-manifest.f78bd26758f85df7fc2f9cf782e34af7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21eb4b7f61e9c62da6140d88b6c50ea",
+    "url": "/credit-card/index.html"
+  },
+  {
+    "revision": "46f2744099db3a5e7a59",
+    "url": "/credit-card/static/css/main.f67255c3.chunk.css"
+  },
+  {
+    "revision": "67af409fea93cbd260bf",
+    "url": "/credit-card/static/js/2.4f419c82.chunk.js"
+  },
+  {
+    "revision": "46f2744099db3a5e7a59",
+    "url": "/credit-card/static/js/main.dfd040ce.chunk.js"
+  },
+  {
+    "revision": "1fc0f6a4eeca7e35f7bd",
+    "url": "/credit-card/static/js/runtime-main.c801e583.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/credit-card/static/media/logo.25bf045c.svg"
+  }
+]);
